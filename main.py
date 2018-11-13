52435b2ed9476480114d6256364bf7f2a555fe12
Helle Gråberg <helle.graaberg@hotmail.com>
2018-11-13 16:29:17 +0100

    test

--- main.py
+++ main.py
@@ -7,10 +7,7 @@ from robodemo import *
 if __name__ == '__main__':
     print("Running BBCon")
 
-    tourist()
-    """
     BBCon = BBCon()
     ZumoButton().wait_for_press()
     while 1:
         BBCon.run_one_timestep()
-    """
