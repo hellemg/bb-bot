ee1029a67e281a4fd5eee3959dfe620b26f8c6a7
Helle Gråberg <helle.graaberg@hotmail.com>
2018-11-13 16:44:42 +0100

    test

--- motors.py
+++ motors.py
@@ -32,6 +32,7 @@ class Motors(Motobs):
     # is the time (in seconds) that the action will persist.
 
     def forward(self, speed=0.25, dur=None):
+        print("welcome to motor forward")
         self.dc = int(self.max * speed)
         self.set_left_dir(0)
         self.set_right_dir(0)
